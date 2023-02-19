2c8f28c99ac4dddac310e5b5107de4347c0e3470
user <your@email.com>
2023-02-19 16:08:58 +0400

    Lesson 4

---
+++ Lesson 4/script.js
@@ -0,0 +1,10 @@
+var p = document.querySelectorAll('p')
+console.log(p)
+var a = document.querySelectorAll('.a')
+console.log(a)
+var id = document.querySelectorAll("#myP")
+console.log(id)
+
+id.addEventListener('click',()=>{
+    console.log("clicked")
+})
---
+++ Lesson 4/server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
+app.use(express.static('.'))
+
+app.get('/',(req,res)=>{
+    res.redirect('index.html')
+})
+
+var messages = []
+
+io.on('connection',(socket)=>{
+    for(var i in messages)
+    socket.emit('display messages',messages[i])
+    socket.on('new message',(msg)=>{
+        messages.push(msg)
+        io.sockets.emit('display messages',msg)
+    })
+})
+
+server.listen(3000)
